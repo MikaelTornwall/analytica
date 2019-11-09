9fb0dd71cc6f4bc16ca79e67b0ac9a99589648f6
Mikael Törnwall <mikael.tornwall@aalto.fi>
2019-11-10 00:23:06 +0200

    paivita pom.xml matematiikkakirjasto ja javafx

--- courseProject/src/main/java/com/mycompany/courseproject/Main.java
+++ courseProject/src/main/java/com/mycompany/courseproject/Main.java
@@ -1,8 +1,10 @@
 package com.mycompany.courseproject;
 
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!");        
     }
 }
